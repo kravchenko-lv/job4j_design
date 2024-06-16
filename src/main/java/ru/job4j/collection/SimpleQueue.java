aff1800099a4eb57175172c784e1d817d08e014b
Kravchenko Liudmila <kravchenko.lv@open.ru>
2024-06-17 02:09:53 +0300

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,39 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> input = new SimpleStack<>();
+    private final SimpleStack<T> output = new SimpleStack<>();
+    private int countIn = 0;
+    private int countOut = 0;
+
+    /* добавьте переменные, если они требуются */
+    //должен возвращать первое значение и удалять его из коллекции.
+    public T poll() {
+        if (countIn == 0 && countOut == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        T value = null;
+        if (countOut > 0 ) {
+            value = output.pop();
+            countOut--;
+        }
+        if (value == null) {
+            while (countIn > 1) {
+                value = input.pop();
+                output.push(value);
+                countIn--;
+                countOut++;
+            }
+            value = input.pop();
+            countIn--;
+        }
+        return value;
+    }
+
+    public void push(T value) {
+        input.push(value);
+        countIn++;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/SimpleQueueTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.NoSuchElementException;
+
+class SimpleQueueTest {
+    private SimpleQueue<Integer> queue;
+
+    @BeforeEach
+    public void setUp() {
+        queue = new SimpleQueue<>();
+        queue.push(1);
+    }
+
+    @Test
+    void whenPushPoll() {
+        assertThat(queue.poll()).isEqualTo(1);
+    }
+
+    @Test
+    void when2PushPoll() {
+        queue.push(2);
+        assertThat(queue.poll()).isEqualTo(1);
+    }
+
+    @Test
+    void when2PushPollPushPoll() {
+        queue.poll();
+        queue.push(2);
+        assertThat(queue.poll()).isEqualTo(2);
+    }
+
+    @Test
+    void whenEmptyPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        assertThatThrownBy(queue::poll)
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessageContaining("Queue is empty");
+    }
+
+    @Test
+    void when2PushPollPushPollEmpty() {
+        queue.poll();
+        queue.push(2);
+        queue.poll();
+        assertThatThrownBy(queue::poll)
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessageContaining("Queue is empty");
+    }
+
+    @Test
+    void whenPushPushPollAndPush() {
+        queue.push(2);
+        queue.poll();
+        queue.push(3);
+        assertThat(queue.poll()).isEqualTo(2);
+    }
+    @Test
+    void whenPushPushPollAndPush3() {
+        queue.push(2);
+        queue.push(3);
+        queue.poll();
+        queue.push(4);
+        queue.push(5);
+        queue.poll();
+        queue.poll();
+        assertThat(queue.poll()).isEqualTo(4);
+    }
+}
